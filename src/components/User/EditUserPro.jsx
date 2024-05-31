410013efa1bc86f36d2f0d4326895e59c93527c1
Aswin K S <aswinkalathur11@gmail.com>
2024-05-31 08:40:09 +0530

    test

--- src/components/User/EditUserPro.jsx
+++ src/components/User/EditUserPro.jsx
@@ -43,14 +43,14 @@ const EditUserPro = () => {
         const imageUpload = async (image) => {
             const result = await profileImageUpload(image)
             console.log('pro img upld result',result);
-            
+
             if(result.message =='Network Error'){
                 toast.error('Something went wrong')
                 setUploading('')
                 return
             }
 
-            if (result) {
+            if (result==='string') {
                 setUploading('Uploaded')
                 toast.success('Profile image updated')
                 dispatch(loginSuccess(result))
