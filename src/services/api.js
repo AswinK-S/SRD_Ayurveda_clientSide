1e137fc0e00c8b2f92278b77becabbca0740747a
Aswin K S <aswinkalathur11@gmail.com>
2024-05-31 09:05:16 +0530

    updated axios api

--- src/services/api.js
+++ src/services/api.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
-console.log('bcknd',import.meta.env.VITE_BACKEND)
-console.log('cldnry',import.meta.env.VITE_CLOUDINARY_ROUTE);
-
 
 const axiosApi = axios.create({
     baseURL:import.meta.env.VITE_BACKEND,withCredentials:true,
-    // baseURL:"http://localhost:3000",withCredentials:true
     
 })
 
 
 const axiosCloudinaryApi= axios.create({
     baseURL:import.meta.env.VITE_CLOUDINARY_ROUTE
-    // baseURL:'https://api.cloudinary.com/v1_1/djmr6njuu/image/upload'
+   
 })
 
 export{
